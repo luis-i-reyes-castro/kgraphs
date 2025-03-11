f919973d2b47ad7c2fc5f5cf09db9b3e5fbc6898
luis-i-reyes-castro <luis.i.reyes.castro@gmail.com>
2025-03-10 19:18:40 -0500

    Adds prompt templates, experiments with new prompt.

--- mistral-ocr-test.py
+++ mistral-ocr-test.py
@@ -1,6 +1,7 @@
 import base64
 import os
 from mistralai import Mistral
+import prompt_templates as pt
 
 def encode_image(image_path):
     """Encode the image to base64."""
@@ -15,8 +16,22 @@ def encode_image(image_path):
         return None
 
 # Path to your image
-# image_path = "/home/luis/DJI_AGRAS_LATINO/rc/t40_t50/spray/2023-05-31_09-11-36_i0.jpg"
-image_path = "/home/luis/DJI_AGRAS_LATINO/rc/t40_t50/spray/2023-09-07_20-38-33_i0.jpg"
+IMAGE_DIR = "/home/luis/DJI_AGRAS_LATINO/rc/t40_t50/"
+# Spray images
+# image_path = IMAGE_DIR + "spray/2023-05-31_09-11-36_i0.jpg"
+# image_path = IMAGE_DIR + "spray/2023-09-07_20-38-33_i0.jpg"
+# image_path = IMAGE_DIR + "spray/2024-01-11_09-36-34_i0.jpg"
+# image_path = IMAGE_DIR + "spray/2023-07-10_14-12-29_i0.jpg"
+# image_path = IMAGE_DIR + "spray/2023-07-23_18-29-39_i0.jpg"
+# image_path = IMAGE_DIR + "spray/2023-07-25_13-45-14_i0.jpg"
+# Prop images
+# image_path = IMAGE_DIR + "prop/2023-06-13_16-33-59_i0.jpg"
+# image_path = IMAGE_DIR + "prop/2023-09-05_07-13-10_i0.jpg"
+# Flight images
+# image_path = IMAGE_DIR + "flight/2023-07-22_10-22-35_i0.jpg"
+# image_path = IMAGE_DIR + "flight/2023-07-25_11-08-08_i0.jpg"
+# image_path = IMAGE_DIR + "flight/2023-08-23_11-05-18_i0.jpg"
+image_path = IMAGE_DIR + "flight/2023-08-26_11-53-42_i0.jpg"
 
 # Getting the base64 string
 base64_image = encode_image(image_path)
@@ -32,7 +47,7 @@ messages = [
         "content": [
             {
                 "type": "text",
-                "text": "The attached image is a photo of a drone remote control screen. Zero, one or more error messages may be visible inside red boxes, either in English or Spanish (but only in one language). Please list the error messages in JSON format according to the following template: [ {'language': fill language, 'num_messages': fill number of messages (zero if none)}, {'message_text': fill first error message text}, ... ]."
+                "text": pt.prompt2
             },
             {
                 "type": "image_url",
